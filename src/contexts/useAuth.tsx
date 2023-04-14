d7889742e15a21c27ffee507e239d77d230acc41
Nilton Hiroyuki Matsumoto <hiroyukims@hotmail.com>
2023-04-14 12:14:56 -0300

    v1: remove arquivos

--- src/contexts/useAuth.tsx
+++
@@ -1,7 +0,0 @@
-// import { useContext } from "react";
-// import { AuthContext } from "./auth/AuthContext"
-
-// export const useAuth = () => {
-//     const context = useContext(AuthContext)
-//     return context;
-// }
